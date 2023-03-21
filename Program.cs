265506f76057d666e84d38302223b699699e5b3d
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-21 14:59:52 +0100

    classi derivate

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿public class UtenteRegistrato
+﻿UtenteRegistrato utentes = new UtenteRegistrato("Gognome", "Bone", "yoyo98email", "gattiPleonastici", "3456776543");
+
+Console.WriteLine($"Codice prodotto: {utentes.Cognome}");
+
+
+public class UtenteRegistrato
 {
     public string Cognome { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Telefono { get; set; }
+    public string Telefono { get; set; }    
+    //public bool Registrato { get; set; } poi vedi se riesci a fare la distinzione tra registrato e non
+
+
+    public UtenteRegistrato(string cognome, string nome, string email, string password, string telefono)
+    {
+        Cognome = cognome;
+        Nome = nome;
+        Email = email;
+        Password = password;
+        Telefono = telefono;
+    }
+
 }
 
 
