3d6d49a973e9f0c95e440887f4317a3f55c51e79
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-21 14:24:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿public class UtenteRegistrato
+{
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Telefono { get; set; }
+}
+
+
+public class Documento
+{
+    public string Codice { get; set; }
+    public string Titolo { get; set; }
+    public string Settore { get; set; }
+    public string Scaffale { get; set; }
+    public string Autore { get; set; }
+}
